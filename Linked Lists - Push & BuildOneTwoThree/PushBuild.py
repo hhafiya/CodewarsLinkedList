8d41f214572054f7dbfbf9a2087730829c907588
Your Name <you@example.com>
2024-04-03 15:39:31 +0300

    changed Node class to import

--- Linked Lists - Push & BuildOneTwoThree/PushBuild.py
+++ Linked Lists - Push & BuildOneTwoThree/PushBuild.py
@@ -1,10 +1,13 @@
+from preloaded import Node
+
+'''
+Node is defined in preloaded like this:
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-    
-    def __repr__(self):
-        return f'Node{self.data, self.next}'
+'''
     
 def push(head, data):
     pushed = Node(data)
@@ -12,9 +15,7 @@ def push(head, data):
     return pushed
 
 def build_one_two_three():
-    # Your code goes here.
-    data_lst = [3, 2, 1]
+    lst = [3, 2, 1]
     head = None
-    for data in data_lst:
+    for data in lst:
         head = push(head, data)
-    return head
