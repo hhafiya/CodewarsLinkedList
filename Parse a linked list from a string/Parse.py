f384499a116f2d2289266626a86b6fa876a5d041
Your Name <you@example.com>
2024-04-03 15:04:11 +0300

    second task

---
+++ Parse a linked list from a string/Parse.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data, next=None): 
+        self.data = data
+        self.next = next
+    
+    def __repr__(self):
+        return f'Node{self.data, self.next}'
+
+def linked_list_from_string(s):
+    s_list = list(reversed(s.split(' -> ')))
+    tail, s_list = None, s_list[1:]
+    current, head = tail, None
+    for i in s_list:
+        head = Node(int(i), current)
+        current = head
+    return head
